23835f3bce6c73644371280e80a5a36ad8929168
Parsha Pourkhomami <parshap+git@gmail.com>
2013-04-01 04:16:59 -0700

    Get page title

--- test.js
+++ test.js
@@ -3,13 +3,16 @@
 var test = require("tape"),
 	pageinfo = require("./");
 
-var URL = "http://en.wikipedia.org/wiki/Fish";
+var URL = "http://en.wikipedia.org/wiki/Fish",
+	TITLE = "Fish - Wikipedia, the free encyclopedia";
 
 test(function(t) {
 	pageinfo(URL, function(err, info) {
 		t.error(err);
 		t.ok(info);
 		t.ok(info.images);
+		t.equal(info.title, TITLE);
+		t.ok(info.icon);
 		t.end();
 	});
 });
